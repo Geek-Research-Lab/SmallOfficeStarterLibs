8ac7a8ffc41a2ab5b0ce2eaf0816087298658d9b
ashumeow <ashumeow@live.com>
2020-02-03 21:09:37 +0300

    update

---
+++ Small Office Starter Libraries/FrontDesk/FrontDesk/Lang_AR.vb
@@ -0,0 +1,13 @@
+﻿''
+'' language : arabic
+''
+Public Module Lang_AR
+    ' type
+    Public Function AR_type()
+        Dim type0 As String = "نوع"
+        Return type0
+    End Function
+End Module
+''
+'' ashumeow
+''
