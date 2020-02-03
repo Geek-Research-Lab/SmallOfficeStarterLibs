0b002da680d02d571b0ee95cc8c7e2315c9bea9e
ashumeow <ashumeow@live.com>
2020-02-03 10:07:49 +0300

    update

---
+++ Small Office Starter Libraries/UserCredentials/UserCredentials/Credentials.vb
@@ -0,0 +1,32 @@
+﻿Public Module Credentials
+    '' front desk
+    Public Function FrontDeskUser()
+        Dim username As String = "frontdesk"
+        Return username
+    End Function
+    Public Function FrontDeskPwd()
+        Dim password As String = "frontdesk"
+        Return password
+    End Function
+    '' hr / payroll
+    Public Function HrPayrollUser()
+        Dim username As String = "hrpayroll"
+        Return username
+    End Function
+    Public Function HrPayrollPwd()
+        Dim password As String = "hrpayroll"
+        Return password
+    End Function
+    '' accounts
+    Public Function AccountsUser()
+        Dim username As String = "accounts"
+        Return username
+    End Function
+    Public Function AccountsPwd()
+        Dim password As String = "accounts"
+        Return password
+    End Function
+End Module
+''
+'' ashumeow
+''
---
+++ Small Office Starter Libraries/UserCredentials/UserCredentials/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Small Office Starter - Libraries (User Credentials)")>
+<Assembly: AssemblyDescription("User Credentials")>
+<Assembly: AssemblyCompany("Geek Research Lab")>
+<Assembly: AssemblyProduct("Small Office Starter - Libraries (User Credentials)")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("ASWINI S (Geek Research Lab)")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("dd23bb66-c21d-40e0-9f35-e3f17d395acb")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
