b8b171602419ceed51779a6720c8037ce42a7d78
ashumeow <ashumeow@live.com>
2020-02-11 12:09:16 +0300

    update

---
+++ Small Office Starter Libraries/FrontDesk/FrontDesk/Home_AR.vb
@@ -0,0 +1,92 @@
+﻿''
+'' front desk - home page - arabic
+''
+Public Module Home_AR
+    ' 0
+    Public Function Home_AR_Type()
+        Return My.Resources.Lang_AR_0.ToString()
+    End Function
+    ' 1
+    Public Function Home_AR_Name()
+        Return My.Resources.Lang_AR_1.ToString()
+    End Function
+    ' 2
+    Public Function Home_AR_Email()
+        Return My.Resources.Lang_AR_2.ToString()
+    End Function
+    '3
+    Public Function Home_AR_Phone()
+        Return My.Resources.Lang_AR_3.ToString()
+    End Function
+    ' 4
+    Public Function Home_AR_OtherContact()
+        Return My.Resources.Lang_AR_4.ToString()
+    End Function
+    ' 5
+    Public Function Home_AR_Description()
+        Return My.Resources.Lang_AR_5.ToString()
+    End Function
+    ' 6
+    Public Function Home_AR_ID()
+        Return My.Resources.Lang_AR_6.ToString()
+    End Function
+    ' 7
+    Public Function Home_AR_DateTime()
+        Return My.Resources.Lang_AR_7.ToString()
+    End Function
+    ' 8
+    Public Function Home_AR_Submit()
+        Return My.Resources.Lang_AR_8.ToString()
+    End Function
+    ' 9
+    Public Function Home_AR_Title()
+        Return My.Resources.Lang_AR_9.ToString()
+    End Function
+    ' 10
+    Public Function Home_AR_GetInfo()
+        Return My.Resources.Lang_AR_10.ToString()
+    End Function
+    ' 11
+    Public Function Home_AR_Enquiry()
+        Return My.Resources.Lang_AR_11.ToString()
+    End Function
+    ' 12
+    Public Function Home_AR_Visitors()
+        Return My.Resources.Lang_AR_12.ToString()
+    End Function
+    ' 13
+    Public Function Home_AR_Contacts()
+        Return My.Resources.Lang_AR_13.ToString()
+    End Function
+    ' 14
+    Public Function Home_AR_Report()
+        Return My.Resources.Lang_AR_14.ToString()
+    End Function
+    ' 15
+    Public Function Home_AR_Settings()
+        Return My.Resources.Lang_AR_15.ToString()
+    End Function
+    ' 16
+    Public Function Home_AR_Appearance()
+        Return My.Resources.Lang_AR_16.ToString()
+    End Function
+    ' 17
+    Public Function Home_AR_Lang()
+        Return My.Resources.Lang_AR_17.ToString()
+    End Function
+    ' 18
+    Public Function Home_AR_CompanyInfo()
+        Return My.Resources.Lang_AR_18.ToString()
+    End Function
+    ' 19
+    Public Function Home_AR_About()
+        Return My.Resources.Lang_AR_19.ToString()
+    End Function
+    ' 20
+    Public Function Home_AR_Exit()
+        Return My.Resources.Lang_AR_20.ToString()
+    End Function
+End Module
+''
+'' ashumeow
+''
---
+++ Small Office Starter Libraries/FrontDesk/FrontDesk/Home_EN.vb
@@ -0,0 +1,92 @@
+﻿''
+'' front desk - home page - english
+''
+Public Module Home_EN
+    ' 0
+    Public Function Home_EN_Type()
+        Return My.Resources.Lang_EN_0.ToString()
+    End Function
+    ' 1
+    Public Function Home_EN_Name()
+        Return My.Resources.Lang_EN_1.ToString()
+    End Function
+    ' 2
+    Public Function Home_EN_Email()
+        Return My.Resources.Lang_EN_2.ToString()
+    End Function
+    '3
+    Public Function Home_EN_Phone()
+        Return My.Resources.Lang_EN_3.ToString()
+    End Function
+    ' 4
+    Public Function Home_EN_OtherContact()
+        Return My.Resources.Lang_EN_4.ToString()
+    End Function
+    ' 5
+    Public Function Home_EN_Description()
+        Return My.Resources.Lang_EN_5.ToString()
+    End Function
+    ' 6
+    Public Function Home_EN_ID()
+        Return My.Resources.Lang_EN_6.ToString()
+    End Function
+    ' 7
+    Public Function Home_EN_DateTime()
+        Return My.Resources.Lang_EN_7.ToString()
+    End Function
+    ' 8
+    Public Function Home_EN_Submit()
+        Return My.Resources.Lang_EN_8.ToString()
+    End Function
+    ' 9
+    Public Function Home_EN_Title()
+        Return My.Resources.Lang_EN_9.ToString()
+    End Function
+    ' 10
+    Public Function Home_EN_GetInfo()
+        Return My.Resources.Lang_EN_10.ToString()
+    End Function
+    ' 11
+    Public Function Home_EN_Enquiry()
+        Return My.Resources.Lang_EN_11.ToString()
+    End Function
+    ' 12
+    Public Function Home_EN_Visitors()
+        Return My.Resources.Lang_EN_12.ToString()
+    End Function
+    ' 13
+    Public Function Home_EN_Contacts()
+        Return My.Resources.Lang_EN_13.ToString()
+    End Function
+    ' 14
+    Public Function Home_EN_Report()
+        Return My.Resources.Lang_EN_14.ToString()
+    End Function
+    ' 15
+    Public Function Home_EN_Settings()
+        Return My.Resources.Lang_EN_15.ToString()
+    End Function
+    ' 16
+    Public Function Home_EN_Appearance()
+        Return My.Resources.Lang_EN_16.ToString()
+    End Function
+    ' 17
+    Public Function Home_EN_Lang()
+        Return My.Resources.Lang_EN_17.ToString()
+    End Function
+    ' 18
+    Public Function Home_EN_CompanyInfo()
+        Return My.Resources.Lang_EN_18.ToString()
+    End Function
+    ' 19
+    Public Function Home_EN_About()
+        Return My.Resources.Lang_EN_19.ToString()
+    End Function
+    ' 20
+    Public Function Home_EN_Exit()
+        Return My.Resources.Lang_EN_20.ToString()
+    End Function
+End Module
+''
+'' ashumeow
+''
