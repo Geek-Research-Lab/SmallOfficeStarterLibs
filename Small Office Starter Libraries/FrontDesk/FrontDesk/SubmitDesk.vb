a56a9d77244c6ddc97b9a19f6779e92bb595b2c4
ashumeow <ashumeow@live.com>
2020-02-17 16:12:55 +0300

    update

---
+++ Small Office Starter Libraries/FrontDesk/FrontDesk/SubmitDesk.vb
@@ -0,0 +1,24 @@
+﻿''
+'' new front desk entry
+''
+Public Module SubmitDesk
+    Public Function FD_Submit(ByVal m1 As String, ByVal m2 As String, ByVal m3 As String, ByVal m4 As String,
+                              ByVal m5 As String, ByVal m6 As String, ByVal m7 As String, ByVal m8 As String)
+        ' m1 - Date / Time
+        ' m2 - ID
+        ' m3 - Type
+        ' m4 - Name
+        ' m5 - Email
+        ' m6 - Phone
+        ' m7 - Other Contact
+        ' m8 - Description
+        ''
+        '' todo : add code
+        ''
+        Dim test0 = m1 + m2 + m3 + m4 + m5 + m6 + m7 + m8
+        Return test0
+    End Function
+End Module
+''
+'' ashumeow
+''
