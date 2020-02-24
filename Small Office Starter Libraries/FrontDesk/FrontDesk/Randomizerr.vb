0c48bce1e765241649248d2f06a0bb69122aab7d
ashumeow <ashumeow@live.com>
2020-02-24 19:09:15 +0300

    update

---
+++ Small Office Starter Libraries/FrontDesk/FrontDesk/Randomizerr.vb
@@ -0,0 +1,18 @@
+﻿''
+'' random id / date
+''
+' random id
+Public Module Randomizerr
+    Dim m_m As New Random
+    Dim mmm As String = ""
+    Public Function RandomID()
+        mmm = m_m.Next(1000, 10000).ToString()
+        Return mmm
+    End Function
+    ''
+    '' todo : add some more code
+    ''
+End Module
+''
+'' ashumeow
+''
