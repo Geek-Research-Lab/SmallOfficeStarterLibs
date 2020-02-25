821749776e199f57752fcc72099d9cc1335386f9
ashumeow <ashumeow@live.com>
2020-02-25 16:19:53 +0300

    init

---
+++ Small Office Starter Libraries/HrPayroll/HrPayroll/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Small Office Starter - Libraries (HR / Payroll)")>
+<Assembly: AssemblyDescription("HR / Payroll Library Module")>
+<Assembly: AssemblyCompany("Geek Research Lab")>
+<Assembly: AssemblyProduct("Small Office Starter - Libraries (HR / Payroll)")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("ASWINI S (Geek Research Lab)")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("1d77b3df-464b-459f-9ad5-fd10eccfea12")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
